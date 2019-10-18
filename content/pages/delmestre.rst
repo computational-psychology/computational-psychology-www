b703af2bb71f4f894ef913dbc4f32f3b8330d8a8
Bianca del Mestre <bianca.delmestre@tu-berlin.de>
2019-10-18 12:10:18 +0200

    changes at biancas computer

---
+++ content/pages/delmestre.rst
@@ -0,0 +1,29 @@
+Bianca del Mestre
+***************************
+
+
+:save_as: delmestre.html
+:url: delmestre.html
+
+
+
+.. container:: twocol
+
+   .. container:: leftside
+
+      - Room: MAR 5.019
+
+      - Email: bianca.delmestre@mail.tu-berlin.de
+
+      - Office hours: Mon-Wed 10:00 a.m. to 12:00 p.m.
+      
+
+   .. container:: rightside
+
+      .. figure:: img/bm_500.png
+		 :width: 25%
+		 :align: right
+		 :alt: Bianca del Mestre
+
+
+
--- publish.sh
+++ publish.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-#make html
+git add content
+
+git commit -a -m "changes at biancas computer"
+
 make publish
 
-# This code here when we had the page hosted in Github pages. Now it's not the case anymore
-#ghp-import output -b gh-pages
-#git push origin gh-pages
-
-# we now have the page hosted in the TU server, wherer we access the folder via AFS
-# AFS should be already configured. Follow instructions here:
-#  https://www.campusmanagement.tu-berlin.de/menue/dienste/daten_server/andrew_file_system/
 
 cp -R output/* /afs/tu-berlin.de/units/Fak_IV/psyco/www/www.psyco/htdocs/
 
