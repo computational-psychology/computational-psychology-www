e9c6f8c78a2c337b5140b13487184a1250f9b880
Guillermo Aguilar <gmo.aguilar.c@gmail.com>
2021-01-20 18:16:48 +0100

    updated HOWTO, using SSH from now on

--- publish_ssh.sh
+++ publish_ssh.sh
@@ -2,4 +2,4 @@
 
 make publish
 
-rsync -a --dry-run output/ guillermo.aguilar@sshgate.tu-berlin.de:/afs/tu-berlin.de/units/Fak_IV/psyco/www/www.psyco/htdocs
+rsync -a --dry-run output/ $1@sshgate.tu-berlin.de:/afs/tu-berlin.de/units/Fak_IV/psyco/www/www.psyco/htdocs
