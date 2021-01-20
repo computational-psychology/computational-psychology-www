6b9413d2e6f4a23e4feca27e8e0f0cf27c8e07a8
Guillermo Aguilar <gmo.aguilar.c@gmail.com>
2021-01-20 18:29:21 +0100

    publish updated, production version

--- publish.sh
+++ publish.sh
@@ -2,4 +2,4 @@
 
 make publish
 
-rsync -a --dry-run output/ $1@sshgate.tu-berlin.de:/afs/tu-berlin.de/units/Fak_IV/psyco/www/www.psyco/htdocs
+rsync -av --delete output/ "$1"@sshgate.tu-berlin.de:/afs/tu-berlin.de/units/Fak_IV/psyco/www/www.psyco/htdocs
